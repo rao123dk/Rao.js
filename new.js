7d562e6917258c91341e9a77a470d5a32308a97a
Dheeraj kumar Rao <dheerajkrao1993@gmail.com>
2017-09-28 18:02:36 +0530

    Getter metod for id

--- new.js
+++ new.js
@@ -1,2 +1,11 @@
-var a = rao("input[type='password']");
-console.log(a);
+//var a = rao("#userame");
+//console.log(a.val());
+
+
+//var b = rao("input[type='text']");
+//console.log(b);
+
+
+var c = rao(".myname");
+console.log(c);
+console.log(c);
--- rao.js
+++ rao.js
@@ -1,20 +1,33 @@
 var Rao = function(selector) {
 	this.selector = selector || null; 
- 	this.element = null; 
+ 	this.element = null;
+ 	this.arr=[];
 };
 
-//every time called
+//main
 rao = function(selector){
 	var elm = new Rao(selector);
 	elm.init();
 	return elm;
 }
 
-// for initialization
+// for initialization and selection
 Rao.prototype.init = function(){
-	this.element = document.querySelector(this.selector);
+	if(this.selector.charAt(0) === '#'){
+		this.element = document.getElementById(this.selector.substr(1));
+	}else if(this.selector.charAt(0) === '.'){
+		//this.element = document.getElementsByClassName(this.selector.substr(1));
+		this.element = document.querySelectorAll(this.selector);//["element"];
+		for(vv in this.element){
+			this.arr.push(vv);
+		}
+		
+	}
 }
+
+
 //for Getting value
-Rao.prototype.val = function(){
-	
+Rao.prototype.val = function(currVal){
+	console.log("currVal:--"+ currVal);
+	return(currVal === undefined ? this.element.value : this.element.value = currVal);
 }
